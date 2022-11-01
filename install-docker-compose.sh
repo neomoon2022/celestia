1edf9864c88244216566398d41b3167d3e0e9f80
root <root@vmi1059232.contaboserver.net>
2022-11-01 14:27:09 +0100

    Install docker compose

---
+++ install-docker-compose.sh
@@ -0,0 +1,5 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod a+x /usr/local/bin/docker-compose
+
+docker-compose --version
